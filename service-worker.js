ba63008a4ee083b2a0b3ee7ec4495281f7e330b0
Saltoleto <fernandosaltoleto@hotmail.com>
2026-01-31 14:36:43 -0300

    Change cache version and update cached file paths
    Updated cache version and modified cached files list.

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,23 @@
-const CACHE_NAME = "solar-cache-v2";
+const CACHE_NAME = "solar-cache-v1";
 const FILES_TO_CACHE = [
   "/controle-pagamentos-energia-solar/",
   "/controle-pagamentos-energia-solar/index.html",
-  "/controle-pagamentos-energia-solar/manifest.json",
-  "/controle-pagamentos-energia-solar/icon-192.png",
-  "/controle-pagamentos-energia-solar/icon-512.png"
+  "/controle-pagamentos-energia-solar/manifest.json"
+  "/controle-pagamentos-energia-solar/icons/icon-192.png",
+  "/controle-pagamentos-energia-solar/icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-});
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
