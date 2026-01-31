a232b0b5b2490546ded992164bbf1bd6409114b1
Saltoleto <fernandosaltoleto@hotmail.com>
2026-01-31 00:07:20 -0300

    Update Firebase configuration and import syntax

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,12 @@
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_DOMINIO",
-  projectId: "SEU_PROJECT_ID",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyCjs2EU9JuVHTxI0ZNIszAdH3mdF7obGnA",
+  authDomain: "controle-pagamentos-3759d.firebaseapp.com",
+  projectId: "controle-pagamentos-3759d",
+  messagingSenderId: "219928011798",
+  appId: "1:219928011798:web:7946650d61cf0bc2528137"
 });
 
 const messaging = firebase.messaging();
@@ -16,7 +16,7 @@ messaging.onBackgroundMessage(payload => {
     payload.notification.title,
     {
       body: payload.notification.body,
-      icon: '/icon.png'
+      icon: "/icon.png"
     }
   );
 });
