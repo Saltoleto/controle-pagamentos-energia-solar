e6cd1d7b92ca3c0db60bba49976139eea5a142b2
Saltoleto <fernandosaltoleto@hotmail.com>
2026-02-04 16:42:42 -0300

    Fix PWA asset paths

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = "solar-cache-v3";
 const FILES_TO_CACHE = [
-  "/controle-pagamentos-energia-solar/",
-  "/controle-pagamentos-energia-solar/index.html",
-  "/controle-pagamentos-energia-solar/manifest.json",
-  "/controle-pagamentos-energia-solar/icons/icon-192.png",
-  "/controle-pagamentos-energia-solar/icons/icon-512.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
